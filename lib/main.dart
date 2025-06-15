803642d112062ed7dbf87da2835fe30651eae60f
bedirhan-1 <bedirhan9828@gmail.com>
2025-06-15 21:45:04 +0300

    counter app

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Counter App",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: CounterScreen(),
+      ),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+
+  // sayacın değerini güncellemek için bir tane
+  // arttırma bir tane de azaltma fonksiyonu yazacağım
+
+  // setState fonksiyonu benim widget'ımı güncellememi sağlıyor.
+  // eğer setState içinde yazmazsam widget güncellenmez.
+
+  // azaltma fonksiyonu:
+  void decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  // arttırma fonksiyonu:
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            counter.toString(),
+            style: TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: () => decrement(),
+                icon: Icon(Icons.remove),
+              ),
+              IconButton(onPressed: () => increment(), icon: Icon(Icons.add)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
